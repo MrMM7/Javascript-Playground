5176900360c582ffe951d772754fb24a0c3bcf9f
MMahallati <96886642+MrMM7@users.noreply.github.com>
2025-01-14 00:40:24 +0330

    Update app.js

--- Clicker-Game/app.js
+++ Clicker-Game/app.js
@@ -2,8 +2,9 @@ const upgrades = document.querySelectorAll(".upgrade");
 
 let cookiecount = document.getElementById("cookie-count");
 const cookiebtn = document.querySelector(".cookie-btn");
+cookiecount.innerHTML = localStorage.getItem("cookies");
 
-let cookies = 0;
+let cookies = localStorage.getItem("cookies") || 0;
 
 let increments = 0;
 
@@ -34,12 +35,32 @@ upgrades[3].addEventListener("click", function () {
     disabler(upgrades[3]);
   }
 });
+upgrades[4].addEventListener("click", function () {
+  if (cookies >= 1000) {
+    cookies = 0;
+    increments = 0;
+    cookiecount.innerHTML = 0;
+    clearInterval();
+    enabler();
+    console.log("Game finished");
+  }
+});
+
+let autoClickerInterval = null; // Variable to store the interval ID
 
 function autoClicker() {
-  setInterval(function () {
+  // Clear any existing interval
+  if (autoClickerInterval) {
+    clearInterval(autoClickerInterval);
+  }
+
+  // Start a new interval
+  autoClickerInterval = setInterval(function () {
     cookies += increments;
     cookiecount.innerHTML = cookies;
   }, 1000);
+
+  // Deduct the appropriate cookie cost
   switch (increments) {
     case 0.25:
       cookies -= 20;
@@ -49,6 +70,7 @@ function autoClicker() {
       break;
     case 5:
       cookies -= 100;
+      break;
   }
 }
 
@@ -57,6 +79,14 @@ function disabler(div) {
   const button = div.lastElementChild;
   button.classList.add("disabled");
   button.disabled = true;
+}
+function enabler() {
+  upgrades.forEach(function (div) {
+    div.classList.remove("purchased");
+    const button = div.lastElementChild;
+    button.classList.remove("disabled");
+    button.disabled = false;
+  });
 }
 
 cookiebtn.addEventListener("click", function () {
@@ -69,3 +99,6 @@ cookiebtn.addEventListener("click", function () {
   if (cookies >= 30) upgrades[3].style.display = "inline";
 });
 
+window.addEventListener("unload", function () {
+  this.localStorage.setItem("cookies", cookies);
+});
