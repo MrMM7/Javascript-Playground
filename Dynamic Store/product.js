048f9b8d148b32c0d6e5ceda31d8327fe8808b39
M.Mahallati <96886642+MrMM7@users.noreply.github.com>
2025-01-26 21:21:45 +0330

    Update product.js

--- Dynamic Store/product.js
+++ Dynamic Store/product.js
@@ -1,9 +1,27 @@
 // ✌ :))
 let allProduct = [
-  { id: 1, src: "images/1.png", name: "Boot", price: 25 },
-  { id: 2, src: "images/2.png", name: "Men Shoes", price: 52 },
-  { id: 3, src: "images/3.png", name: "Women Shoes", price: 99 },
-  { id: 4, src: "images/4.png", name: "Men Shoes 2", price: 22 },
+  { id: 1, src: "images/1.png", name: "Boot", price: 25, desc: "Lorem Iptum" },
+  {
+    id: 2,
+    src: "images/2.png",
+    name: "Men Shoes",
+    price: 52,
+    desc: "Lorem Iptum",
+  },
+  {
+    id: 3,
+    src: "images/3.png",
+    name: "Women Shoes",
+    price: 99,
+    desc: "Lorem Iptum",
+  },
+  {
+    id: 4,
+    src: "images/4.png",
+    name: "Men Shoes 2",
+    price: 22,
+    desc: "Lorem Iptum",
+  },
 ];
 
 let urlParameter = new URLSearchParams(location.search);
@@ -20,7 +38,7 @@ const image = document.querySelector("img");
 image.src = productFinder.src;
 
 const pElement = document.querySelector("p");
-pElement.innerHTML += `(${productFinder.name})`;
+pElement.innerHTML = productFinder.desc;
 
 const backBtn = document.querySelector("button");
 
